9752913d51806877f5fdb6d95969aa15bb346431
颜海镜 <yanhaijing@yeah.net>
2018-11-01 22:05:23 +0800

    doc: add test

--- test/test.js
+++ test/test.js
@@ -1,19 +1,70 @@
 var expect = require('expect.js');
 
-var base = require('../dist/index.js');
+var parse = require('../dist/index.js').parse;
+var stringify = require('../dist/index.js').stringify;
 
 describe('单元测试', function() {
     this.timeout(1000);
 
-    describe('功能1', function() {
-        it('相等', function() {
-            expect(base.name).to.equal('base');
+    describe('parse', function() {
+        it('param check', function() {
+            expect(function () { parse({}) }).to.throwException('parse: first param must is string');
+        })
+
+        it('基本测试', function() {
+            var a = parse('a=1');
+            expect(a).to.eql({ a: '1'});
+
+            var a = parse('a=1&b=2&c=3&d=4');
+            expect(a).to.eql({ a: '1', b: '2', c: '3', d: '4'});
         });
+
+        it('option', function() {
+            var a = parse('a:1+b:2+c:3', {sep: '+', eq: ':'});
+            expect(a).to.eql({ a: '1', b: '2', c: '3'});
+        });
+
+        it('option.ignoreQueryPrefix', function() {
+            var a = parse('?a=1', { ignoreQueryPrefix: false });
+            expect(a).to.eql({ a: '1'});
+
+            var a = parse('?a=1', { ignoreQueryPrefix: true });
+            expect(a).to.eql({ '?a': '1'});
+        });
+
+        it('option.decode', function() {
+            var a = parse('a=%3D')
+            expect(a).to.eql({ a: '=' });
+
+            var a = parse('a=%3D', { decode: function (x) {return x} })
+            expect(a).to.eql({ a: '%3D'});
+        })
     });
 
-    describe('功能2', function() {
-        it('不相等', function() {
-            expect(base.name).not.to.equal(1);
+    describe('stringify', function() {
+        it('param check', function() {
+            expect(function () { stringify('') }).to.throwException('stringify: first param must is object');
+        })
+
+        it('基本测试', function() {
+            var a = stringify({ a: '1'});
+            expect(a).to.eql('a=1');
+
+            var a = stringify({ a: '1', b: '2', c: '3', d: '4'});
+            expect(a).to.eql('a=1&b=2&c=3&d=4');
         });
+
+        it('option', function() {
+            var a = stringify({ a: '1', b: '2', c: '3'}, {sep: '+', eq: ':'});
+            expect(a).to.eql('a:1+b:2+c:3');
+        });
+
+        it('option.encode', function() {
+            var a = stringify({ a: '='})
+            expect(a).to.eql('a=%3D');
+
+            var a = stringify({ a: '='}, { encode: function (x) {return x} })
+            expect(a).to.eql('a==');
+        })
     });
 });
